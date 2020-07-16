e7ba488f74339bd24cd858d737294c51c6833a07
Victor Oliveira <vt.victoroliveira@gmail.com>
2020-07-16 19:07:50 -0300

    adding lista 02  haskell course

--- Main.hs
+++ Main.hs
@@ -1,32 +1,71 @@
--- exercise 01
-{-
-1.2 :: Fractional a => a
+-- Exercicio 1
+data Moeda = Real | Dolar | Euro deriving (Show, Eq)
+data Dinheiro = Dinheiro { getValor :: Double,
+                           getMoeda :: Moeda
+                         } deriving (Show)
+
+cambio :: Dinheiro -> Moeda -> Dinheiro 
+cambio dinheiro moeda
+    | moeda' == Dolar && moeda == Real = Dinheiro (valor*5) moeda
+    | moeda' == Dolar && moeda == Euro = Dinheiro (valor*0.9) moeda
+    | moeda' == Real && moeda == Dolar = Dinheiro (valor/5) moeda
+    | moeda' == Real && moeda == Euro = Dinheiro (valor*0.18) moeda
+    | moeda' == Euro && moeda == Dolar = Dinheiro (valor*1.12) moeda
+    | moeda' == Euro && moeda == Real = Dinheiro (valor*5.7) moeda
+    | otherwise = dinheiro
+    where
+        moeda' = getMoeda dinheiro
+        valor = getValor dinheiro
+
+-- Exercicio 2
+data Tree a = Leaf a | Node (Tree a) a (Tree a) deriving (Show)
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
 
 
+instance Foldable Tree where
+    foldr f z (Leaf a) = f a z
+    foldr f z (Node l y r) = f y (foldr f (foldr f z l) r)
 
-    
--}
 
-sq :: Int -> Int -> Int
-sq x y = x*x + y*y
+-- Exercicio 3
+data Op = And | Or
 
-max' x y = if x > y then x else y
+instance Show Op where
+    show And = "&&"
+    show Or = "||"
 
-zip' :: [a] -> [b] -> [(a,b)]
-zip' xs [] = []
-zip' [] ys = []
-zip' (x:xs) (y:ys) = [(x,y)] ++ zip' xs ys
+data Expr = Const Bool | Expr Expr Op Expr
+instance Show Expr where
+    show (Const True) = "T"
+    show (Const False) = "F"
+    show (Expr x Or y) = "(" ++ (show x) ++ " " ++ (show Or) ++ " " ++ (show y) ++ ")"
+    show (Expr x And y) = (show x) ++ " " ++ (show And) ++ " " ++ (show y)
 
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' _ [] = []
-filter' f (x:xs) = if f x then x:filter' f xs else filter' f xs
 
-length' :: [a] -> Int
-length' [] = 0
-length' (x:xs) = 1 + length' xs
+eval :: Expr -> Bool
+eval (Const x)  = x
+eval (Expr (Const True) And (Const True)) = eval (Const True)
+eval (Expr _ And _) = eval (Const False)
+eval (Expr (Const False) Or (Const False)) = eval (Const False)
+eval (Expr _ Or _) = eval (Const True)
 
--- f :: Int -> [a] -> Int
--- f acc (x:xs) = acc + f acc xs
+-- Exercicio 4
+data Matriz a = Matriz [[a]] deriving (Show)
 
-length'' :: [a] -> Int
-length'' xs = foldl (\acc elem -> acc + 1) 0 xs
+instance Functor Matriz where
+    fmap f (Matriz a) = Matriz (fmap (fmap f) a)
+
+data Request a = Loading | Error | Success a deriving (Show)
+
+instance Functor Request where
+    fmap f Loading = Loading
+    fmap f Error = Error
+    fmap f (Success a) = Success (f a)
+
+instance Applicative Request where
+    pure = Success
+    Loading <*> _ = Loading
+    Error <*> _ = Error
+    _ <*> Loading = Loading
+    _ <*> Error = Error
+    (Success f) <*> smt = fmap f smt
